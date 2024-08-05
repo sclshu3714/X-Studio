648ec60b7de6212dfcd3e57bfa91f6071a960b94
wxz <sclshu3714@163.com>
2024-08-05 21:17:37 +0800

    ==============

---
+++ src/XStudio.App/AppModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace XStudio.App;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class AppModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddSingleton<MainWindow>();
+    }
+}
