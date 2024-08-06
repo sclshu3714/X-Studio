3890c547c0a458d31ffaf99e6fdc9e6e17dee846
wxz <sclshu3714@163.com>
2024-08-06 22:00:34 +0800

    ===========

---
+++ src/XStudio.App/Models/Themes/ThemeItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XStudio.App.Models.Themes
+{
+    public class ThemeItem
+    {
+        public string DisplayName { get; set; } = string.Empty;
+
+        public string LightName { get; set; } = string.Empty;
+
+        public string DarkName { get; set; } = string.Empty;
+    }
+}
---
+++ src/XStudio.App/Service/Themes/IThemeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using XStudio.App.Models.Themes;
+
+namespace XStudio.App.Service.Themes
+{
+    public interface IThemeService
+    {
+        ObservableCollection<ThemeItem> ThemeItems { get; set; }
+
+        string GetCurrentName();
+
+        void SetTheme(string themeName);
+
+        void SetThemeMode();
+
+        void SetCurrentTheme(DependencyObject dependency);
+    }
+}
