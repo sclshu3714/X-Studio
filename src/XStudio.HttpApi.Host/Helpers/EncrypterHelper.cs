29d444891cb65e069c1bbbbc7a985ca97a3d04c9
wxz <sclshu3714@163.com>
2024-08-08 16:40:41 +0800

    =================

--- src/XStudio.HttpApi.Host/Helpers/EncrypterHelper.cs
+++ src/XStudio.HttpApi.Host/Helpers/EncrypterHelper.cs
@@ -1,12 +1,16 @@
 ﻿using System.Text;
 using System;
-using SecurityDriven.Inferno;
 using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace XStudio.Helpers
 {
     public static class EncrypterHelper
     {
+        #region AES加解密算法
         private const string EncryptionKey = "xstudio_web_encryptionkey";// "YourEncryptionKey"; // 替换为您的加密密钥
         private static readonly byte[] IV = new byte[16]; // 使用默认的IV向量
         /// <summary>
@@ -21,8 +25,11 @@ namespace XStudio.Helpers
                 byte[] key = Encoding.UTF8.GetBytes(EncryptionKey);
                 Array.Resize(ref key, 32);
                 byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
-                byte[] encryptedBytes = SuiteB.Encrypt(plainBytes, key, IV);
-                return Convert.ToBase64String(encryptedBytes);
+                IBufferedCipher cipher = CipherUtilities.GetCipher("AES/CTR/PKCS7Padding");
+                cipher.Init(true, new ParametersWithIV(ParameterUtilities.CreateKeyParameter("AES", key), IV));
+                byte[] encryptedBytes = cipher.DoFinal(plainBytes);
+                string encryptedText = Convert.ToBase64String(encryptedBytes);
+                return encryptedText;
             }
             catch (Exception ex)
             {
@@ -41,13 +48,15 @@ namespace XStudio.Helpers
             {
                 byte[] key = Encoding.UTF8.GetBytes(EncryptionKey);
                 Array.Resize(ref key, 32);
-                byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
-                byte[] decryptedBytes = SuiteB.Decrypt(encryptedBytes, key, IV);
-                if (decryptedBytes == null)
+                byte[] ciphertext = Convert.FromBase64String(encryptedText);
+                IBufferedCipher cipher = CipherUtilities.GetCipher("AES/CTR/PKCS7Padding");
+                cipher.Init(false, new ParametersWithIV(ParameterUtilities.CreateKeyParameter("AES", key), IV));
+                byte[] plaintext = cipher.DoFinal(ciphertext);
+                if (plaintext == null)
                 {
                     return "解密错误";
                 }
-                return Encoding.UTF8.GetString(decryptedBytes);
+                return System.Text.Encoding.UTF8.GetString(plaintext);
             }
             catch (Exception ex)
             {
@@ -55,20 +64,7 @@ namespace XStudio.Helpers
             }
         }
 
-        /// <summary>
-        /// 验证秘钥是否正确
-        /// </summary>
-        /// <param name="encryptedText"></param>
-        /// <returns></returns>
-        public static bool Authenticate(string encryptedText)
-        {
-            byte[] key = Encoding.UTF8.GetBytes(EncryptionKey);
-            Array.Resize(ref key, 32);
-            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
-            return SuiteB.Authenticate(key, encryptedBytes);
-        }
 
-        
         /// <summary>
         /// AES加密
         /// </summary>
@@ -108,5 +104,7 @@ namespace XStudio.Helpers
                 return Encoding.UTF8.GetString(decryptedBytes);
             }
         }
+        #endregion
+
     }
 }
