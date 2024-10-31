dd85f976a481aa7165e4410ea8f5b796095227d3
wxz <sclshu3714@163.com>
2024-10-31 21:15:32 +0800

    ====================

---
+++ src/XStudio.App/ViewModel/Module/Schools/SectionViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XStudio.App.ViewModel.Module.Schools {
+    public class SectionViewModel : ViewModelBase {
+        private int _order = 0;
+        private string _code = string.Empty;
+        private string _name = string.Empty;
+
+        private List<string> defaultSelectList = new List<string>() { "早晨", "上午", "中午", "下午", "晚上" };
+        /// <summary>
+        /// 序号
+        /// </summary>
+        [Description("序号")]
+        [JsonProperty("order")]
+        public int Order {
+            get => _order;
+            set => SetProperty(ref _order, value);
+        }
+
+        /// <summary>
+        /// 时段编号
+        /// </summary>
+        [Description("编码")]
+        [JsonProperty("code")]
+        public string Code {
+            get => _code;
+            set => SetProperty(ref _code, value);
+        }
+
+        /// <summary>
+        /// 学校名称
+        /// </summary>
+        [Description("名称")]
+        [JsonProperty("name")]
+        public string Name {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        [JsonIgnore]
+        public List<string> DefaultSelectList {
+            get => defaultSelectList;
+            set => SetProperty(ref defaultSelectList, value);
+        }
+    }
+}
